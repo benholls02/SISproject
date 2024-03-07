4637b03ccbea35663f39927bb016578871aed50f
ben.hollingsworth <ben.hollingsworth@MU-C1-18.mullen.pvt.k12.co.us>
2024-03-07 09:21:14 -0700

    sub menu

---
+++ SIS/src/subMenus.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class subMenus {
+	
+			public static void addStudent() {
+		
+				System.out.println("New student?/n Okay, please type in first name, last name followed by their GPA.");
+				String fName = 
+	}
+			public static void removeStudent() {
+				
+			}
+
+}
