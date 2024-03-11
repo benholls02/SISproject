384a6c808933ce77b9e40c50802275e7db2145d4
maximilian.obrecht <maximilian.obrecht@MU-C1-09.mullen.pvt.k12.co.us>
2024-03-11 08:45:47 -0600

    Database stuff

--- SIS/src/Database.java
+++ SIS/src/Database.java
@@ -1,5 +1,22 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class Database
 	{
+		static ArrayList<Student> database = new ArrayList<Student>();
+		
+		public static void fillArrayList() throws FileNotFoundException
+		{
+			Scanner file = new Scanner(new File("studentList.txt"));
+			while(file.hasNext())
+				{
+					database.add(new Student(file.next(),file.next(),file.next(),file.next(),file.next()));
+				}
+			
+		}
+		
 		
 	}
--- SIS/src/Main.java
+++ SIS/src/Main.java
@@ -1,13 +1,15 @@
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
 public class Main
 	{
-		public static void main(String[] args)
+		public static void main(String[] args) throws FileNotFoundException
 			{
 
 				
-				System.out.println("Hello World");
+				//System.out.println("Hello World");
 				//Initial Commit
+				Database.fillArrayList();
 
 			}
 
