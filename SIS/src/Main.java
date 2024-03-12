c47f75d9974ee765306ceaa0a4f94804ad3f3065
maximilian.obrecht <maximilian.obrecht@MU-C1-09.mullen.pvt.k12.co.us>
2024-03-12 08:09:38 -0600

    Text file in the correct place

--- SIS/src/Main.java
+++ SIS/src/Main.java
@@ -1,7 +1,7 @@
 import java.io.File;
 import java.io.IOException;
-//import java.util.ArrayList;
-//import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Scanner;
 public class Main
 	{
 		public static void main(String[] args) throws IOException
